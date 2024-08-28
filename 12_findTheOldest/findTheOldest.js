12991537548aa4d959ae5e0ff9b330771dab335b
adiluexe <adiluexe@gmail.com>
2024-08-28 10:33:03 +0800

    Add solution for findTheOldest.js
    - This one was relatively straightforward, except for line 9 to 14
    - Basically it starts with null which evaluates to true, so the oldest person is the next real value
    - Then it compares if the age of the current person is greater than the age of the oldest person so far in the loop
    - If it is, it updates the oldest person
    - This line `return { ...person, age }` uses an object spread that returns a new object that includes all properties of the new oldest person
    - I still don't get it :3, but hey it works

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldest, person) => {
+    const currentYear = new Date().getFullYear();
 
+    const deathYear = person.yearOfDeath || currentYear;
+
+    const age = deathYear - person.yearOfBirth;
+
+    if (!oldest || age > oldest.age) {
+      return {
+        ...person,
+        age,
+      };
+    }
+
+    return oldest;
+  }, null);
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
